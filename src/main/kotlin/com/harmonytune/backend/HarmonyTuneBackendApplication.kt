17d4ee51e24c573e206bd828c65c971dd3ad8bc1
RyoTonegawa <tonegawa622@gmail.com>
2025-02-05 23:09:23 +0900

    spring initializr

---
+++ src/main/kotlin/com/harmonytune/backend/HarmonyTuneBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.harmonytune.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HarmonyTuneBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<HarmonyTuneBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/harmonytune/backend/HarmonyTuneBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.harmonytune.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HarmonyTuneBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
