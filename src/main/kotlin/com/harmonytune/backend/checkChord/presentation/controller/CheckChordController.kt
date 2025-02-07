c065c6ca0c74bbdda6f333a27424da6092d4d872
RyoTonegawa <tonegawa622@gmail.com>
2025-02-07 16:43:21 +0900

    add gitIgnore

--- src/main/kotlin/com/harmonytune/backend/checkChord/presentation/controller/CheckChordController.kt
+++ src/main/kotlin/com/harmonytune/backend/checkChord/presentation/controller/CheckChordController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController  
 
 @RestController
-@RequestMapping("/check-chord")
+@RequestMapping("/chord")
 class CheckChordController(
     private val checkChordUseCase: CheckChordUseCase
 ) {
